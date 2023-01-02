3b8db95b11d7f0d45aaaf23b9a08ad65962680c2
J Seymour <JustJakeSeymour@gmail.com>
2023-01-02 10:07:10 -0700

    passing user story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Studio Index" do
   before(:each) do
-    studio_1 = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
-    studio_2 = Studio.create!(name: 'New Line Cinema', location: 'New York City')
-    studio_3 = Studio.create!(name: 'Disney', location: 'Studio City')  
+    @studio_1 = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    @studio_2 = Studio.create!(name: 'New Line Cinema', location: 'New York City')
+    @studio_3 = Studio.create!(name: 'Disney', location: 'Studio City')  
 
-    movie_1 = studio_3.movies.create!(title: 'Toy Story', creation_year: '1995', genre: 'animation')
-    movie_2 = studio_3.movies.create!(title: 'Aristocats', creation_year: '1985', genre: 'animation')
-    movie_3 = studio_2.movies.create!(title: 'Lord of the Rings', creation_year: '2000', genre: 'action')
-    movie_4 = studio_2.movies.create!(title: 'Elf', creation_year: '2004', genre: 'holiday')
-    movie_5 = studio_1.movies.create!(title: 'Jurassic Park', creation_year: '1993', genre: 'action')
-    movie_6 = studio_1.movies.create!(title: 'Despicable Me', creation_year: '2010', genre: 'animation')
+    @movie_1 = @studio_3.movies.create!(title: 'Toy Story', creation_year: '1995', genre: 'animation')
+    @movie_2 = @studio_3.movies.create!(title: 'Aristocats', creation_year: '1985', genre: 'animation')
+    @movie_3 = @studio_2.movies.create!(title: 'Lord of the Rings', creation_year: '2000', genre: 'action')
+    @movie_4 = @studio_2.movies.create!(title: 'Elf', creation_year: '2004', genre: 'holiday')
+    @movie_5 = @studio_1.movies.create!(title: 'Jurassic Park', creation_year: '1993', genre: 'action')
+    @movie_6 = @studio_1.movies.create!(title: 'Despicable Me', creation_year: '2010', genre: 'animation')
   end
 
   describe "User Story 1" do
@@ -32,8 +32,8 @@ RSpec.describe "Studio Index" do
       visit '/studios'
       # And under each studio I see all of the studio's movies
       # including the movie's title, creation year, and genre
-      within("##{studio_1.id}") do
-        expect(page).to have_content("Jurrassic Park")
+      within("##{@studio_1.id}") do
+        expect(page).to have_content("Movies:\nJurassic Park")
         expect(page).to have_content("Year: 1993")
         expect(page).to have_content("Genre: Action")
         expect(page).to have_content("Despicable Me")
@@ -42,23 +42,23 @@ RSpec.describe "Studio Index" do
         expect(page).to_not have_content("Elf")
       end
       
-      within("##{studio_2.id}") do
+      within("##{@studio_2.id}") do
         expect(page).to have_content("Lord of the Rings")
         expect(page).to have_content("Year: 2000")
         expect(page).to have_content("Genre: Action")
         expect(page).to have_content("Elf")
         expect(page).to have_content("Year: 2004")
         expect(page).to have_content("Genre: Holiday")
-        expect(page).to_not have_content("Jurrassic Park")
+        expect(page).to_not have_content("Jurassic Park")
       end
       
-      within("##{studio_3.id}") do
+      within("##{@studio_3.id}") do
         expect(page).to have_content("Toy Story")
         expect(page).to have_content("Year: 1995")
         expect(page).to have_content("Genre: Animation")
         expect(page).to have_content("Aristocats")
         expect(page).to have_content("Year: 1985")
-        expect(page).to have_content("Genre: Animaiton")
+        expect(page).to have_content("Genre: Animation")
         expect(page).to_not have_content("Lord of the Rings")
       end
     end
